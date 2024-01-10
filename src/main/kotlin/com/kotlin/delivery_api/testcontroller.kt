718ba7bf52e28702d56bbae91e0091764d775532
work <qkqhemforhs1@gmail.com>
2024-01-10 16:26:44 +0900

    진행

--- src/main/kotlin/com/kotlin/delivery_api/testcontroller.kt
+++ src/main/kotlin/com/kotlin/delivery_api/testcontroller.kt
@@ -1,4 +1,12 @@
 package com.kotlin.delivery_api
 
-class testcontroller {
+import com.TestService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class testcontroller (
+        val text2 : TestService,
+){
+
 }
---
+++ src/main/kotlin/com/testServie.kt
@@ -0,0 +1,7 @@
+package com
+
+import org.springframework.stereotype.Service
+
+@Service
+class TestService {
+}
